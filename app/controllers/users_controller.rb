324479d2d2a1d392f9b86d0c66a21fdc7fe47ae2
TanakaMana <makkuromyaon@gmail.com>
2018-06-10 14:44:33 +0900

    [create] ユーザー情報編集ページを作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,31 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
+
   def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+     redirect_to edit_user_path, notice: 'ユーザー情報を更新しました。'
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(
+      :email,
+      :name,
+      :calendar_iframe
+    )
   end
 end
