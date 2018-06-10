56eb0fbc8ef798f1167b3fe21ed805aba8ce0aad
TanakaMana <makkuromyaon@gmail.com>
2018-06-10 13:41:12 +0900

    [set] deviseの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'reflections#index'
+  devise_for :users
   resources :reflections
+  resources :users
 end
