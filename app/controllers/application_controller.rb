be3844a2c2af2172396ffa05c999162cedf7f945
TanakaMana <makkuromyaon@gmail.com>
2018-08-05 13:29:00 +0900

    [add] 本番環境にBasic認証を追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  http_basic_authenticate_with :name => ENV['BASIC_USERNAME'], :password => ENV['BASIC_PASSWORD'] if Rails.env == "production"
 end
